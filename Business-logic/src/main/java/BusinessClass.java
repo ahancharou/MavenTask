bab492f00ecb82086d820d9dd74100b9cbf4b317
ahancharou <aliaksei_hancharou@epam.com>
2016-03-04 19:36:10 +0300

    Created structure

---
+++ Business-logic/src/main/java/BusinessClass.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Aliaksei_Hancharou on 3/4/2016.
+ */
+public class BusinessClass {
+
+    public void BusinessLogic (){
+        DAOClass dao = new DAOClass();
+        dao.workOnDAO();
+    }
+}
---
+++ DAO/src/main/java/DAOClass.java
@@ -0,0 +1,12 @@
+
+/**
+ * Created by Aliaksei_Hancharou on 3/4/2016.
+ */
+public class DAOClass {
+
+    public void workOnDAO() {
+        // some DAO logic
+    }
+
+
+}
